78c1b4cf2f4d08a18078905c909ba1bf155cdde9
delibre <viacheslav.bylo@gmail.com>
2023-08-03 16:56:47 +0200

    first commit

---
+++ src/main/java/com/ledikom/ledikom/LedikomApplication.java
@@ -0,0 +1,13 @@
+package com.ledikom.ledikom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedikomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedikomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ledikom/ledikom/LedikomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledikom.ledikom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedikomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

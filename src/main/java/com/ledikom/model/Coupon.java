9eb1a090a07e00579987d971381328c4f49700d9
hlebshypulahub <glebszypula1997@gmail.com>
2023-08-14 20:05:02 +0200

    refactor coupons, generify

--- src/main/java/com/ledikom/model/Coupon.java
+++ src/main/java/com/ledikom/model/Coupon.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,11 +21,19 @@ public class Coupon {
     private String text;
     private String name;
     private int discount;
+    private LocalDateTime expiryDateTime;
 
     public Coupon(final String text, final String name, final int discount) {
         this.text = text;
         this.name = name;
         this.discount = discount;
+    }
+
+    public Coupon(final String text, final String name, final int discount, final LocalDateTime expiryDateTime) {
+        this.text = text;
+        this.name = name;
+        this.discount = discount;
+        this.expiryDateTime = expiryDateTime;
     }
 
     @ManyToMany(mappedBy = "coupons")
